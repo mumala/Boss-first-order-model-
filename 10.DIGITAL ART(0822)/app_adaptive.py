71ce120aec8805419ae83cc01a1b3afd919cf813
mumala <44835468+mumala@users.noreply.github.com>
2020-08-24 17:41:25 +0900

    style transfer 구현 완료버전
    수정할 필요있는 부분 : 연속 실행시 transfer를 여러개 하므로 실행시간 길어짐, transfer 하는 동안 로딩화면 출력할 필요 있음

---
+++ 10.DIGITAL ART(0822)/app_adaptive.py
@@ -0,0 +1,4 @@
+import os
+
+if __name__ == '__main__':
+    os.system("python main.py --model_name=model_van-gogh --phase=inference --image_size=1280 --ii_dir ./my_photographs1/ --save_dir=./save_processed_images_here/")
